2f7cbe6ebf3994362be5efab50fb5c6d63c54cda
dejanv <dejanvojneski82@gmail.com>
2026-02-22 13:17:17 +0100

    Update main.cpp

--- Divide&Conquer/Subarray/main.cpp
+++ Divide&Conquer/Subarray/main.cpp
@@ -1,1 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i=0;i<n;i++) {
+        cin >> arr[i];
+    }
+
+    int maxSoFar=arr[0];
+    int globalMax=arr[0];
+
+    int start=0 , end=0 , curStart=0;
+
+    for (int i=1;i<n;i++) {
+        if (maxSoFar < 0) {
+            maxSoFar=arr[i];
+            curStart = i;
+        }
+        else {
+            maxSoFar+=arr[i];
+        }
+        if (maxSoFar > globalMax) {
+            globalMax=maxSoFar;
+            start=curStart;
+            end=i;
+        }
+    }
+
+    cout << "Max subarray sum is: " << globalMax << endl;
+    cout << "Array is: " ;
+    for (int i=start ; i<=end; i++) {
+        cout << arr[i] << " " ;
+    }
+}
